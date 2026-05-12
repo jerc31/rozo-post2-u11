414fcb4cd3bcf2285708a01680e4073bcd223b72
Jhoseth Esneider Rozo Carrillo <jhoseth331@gmail.com>
2026-05-12 18:17:02 -0500

    feat: configurar logback-spring.xml con consola y archivo rotativo

--- src/main/java/com/empresa/catalogo/repository/ProductoRepository.java
+++ src/main/java/com/empresa/catalogo/repository/ProductoRepository.java
@@ -5,6 +5,6 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductoRepository extends JpaRepository<Producto, Long> {
-
+    // Consulta personalizada para obtener solo productos activos
     List<Producto> findByActivoTrue();
 }
